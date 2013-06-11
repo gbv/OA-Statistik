32f28897b5c5b99e8ec89b160942eca0afb04164
Marco Recke <recke@gbv.de>
2013-06-11 13:37:56 +0000

    --------- V 1.3.5b ---------------------- - removed a stump for harvest history

--- data-provider.beta/config.php
+++ data-provider.beta/config.php
@@ -12,14 +12,14 @@
 
 $config=array(
         'repo_name'            => 'OA-Statistik DEMO Data Provider',
-        'db_uri'               => 'mysql:host=localhost;dbname=oas_data_provider_demo',
+        'db_uri'               => 'mysql:host=localhost;dbname=oas_data_provider',
         'db_table'             => 'contextobjects',
         // postfix of 'tablename'. name of the table with the harvesthistory.
         // example with tablename 'contextobjects' and harvesthistory-postfix 'harvesthistory' (standardvalues): 
         // "contextobjects_harvesthistory"
         'harvesthistory'       => 'harvesthistory',
-        'db_user'              => 'dbuser',
-        'db_password'          => 'dbpassword',
+        'db_user'              => 'db_user',
+        'db_password'          => 'db_password',
         'oai_base_url'         => 'http://oa-statistik.sub.uni-goettingen.de/demo-data-provider/',
         'oai_server_admin'     => 'mimkes@sub.uni-goettingen.de',
         'oai_identifier_prefix'=> 'oai:oa-statistik.sub.uni-goettingen.de:demo',
